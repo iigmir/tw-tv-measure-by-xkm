ea8a760ae78d6e7ddfe509a4a9b3dda967003739
iigmir <roc120j@gmail.com>
2019-04-25 02:04:48 +0800

    encode issue fixed!!!!!

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,27 @@
+require "net/http"
+
 class ApiController < ApplicationController
     def index
-        # Request
+        # api_id = params[:id]
+        api = "http://www1.xkm.com.tw/hr/DATA/HR" + params[:id] + ".htm"
+        url = URI.parse(api)
+        req = Net::HTTP::Get.new(url.to_s)
+        res = Net::HTTP.start(url.host, url.port) {|http|
+            http.request(req)
+        }
+        ic = res.body.encode("utf-8","big5")
+        render :json => {
+            :request_id => params[:id],
+            :source_html => ic
+        }
+    end
+    def http_test
+        url = URI.parse("http://www.example.com/index.html")
+        req = Net::HTTP::Get.new(url.to_s)
+        res = Net::HTTP.start(url.host, url.port) {|http|
+            http.request(req)
+        }
+        # puts res.body
+        render :json => { body: res.body }
     end
 end
