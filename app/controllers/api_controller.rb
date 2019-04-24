99f67cf394341639c84d98b8073cbf35ec08e7f8
iigmir <roc120j@gmail.com>
2019-04-25 02:22:18 +0800

    Use HTML from Nokogiri instread

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,21 +1,19 @@
 require "net/http"
+require "nokogiri"
+require "open-uri"
 
 class ApiController < ApplicationController
     def index
-        # api_id = params[:id]
-        api = "http://www1.xkm.com.tw/hr/DATA/HR" + params[:id] + ".htm"
-        url = URI.parse(api)
-        req = Net::HTTP::Get.new(url.to_s)
-        res = Net::HTTP.start(url.host, url.port) {|http|
-            http.request(req)
-        }
-        ic = res.body.encode("utf-8","big5")
         # https://ruby-china.org/topics/101
         # https://stackoverflow.com/questions/33270851/how-can-i-use-nethttp-to-download-a-file-with-utf-8-characters-in-it
         # Encoding::UndefinedConversionError ("\xA7" from ASCII-8BIT to UTF-8):
+        api = "http://www1.xkm.com.tw/hr/DATA/HR" + params[:id] + ".htm"
+        doc = Nokogiri::HTML(open(api))
+        john = doc.css("table td")
         render :json => {
-            :request_id => params[:id],
-            :source_html => ic
+            request_id: params[:id],
+            # source_html: doc
+            data: john
         }
     end
     def http_test
