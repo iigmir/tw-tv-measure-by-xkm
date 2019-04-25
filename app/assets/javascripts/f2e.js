aebee7550594efb4dfdb2bde6a6b982ee84a84c2
iigmir <roc120j@gmail.com>
2019-04-25 23:36:12 +0800

    data compelted

--- app/assets/javascripts/f2e.js
+++ app/assets/javascripts/f2e.js
@@ -3,9 +3,50 @@
 
 $( document ).ready( () => main() );
 
-function parse_text( data )
+function parse_text( res_data )
 {
-    console.log( data );
+    function render_tbody({ item })
+    {
+        let render_thtd = (item_keys_item, item_keys_index, meta_data) =>
+        {   // <th scope="col">${ value0 }</th><td>${ value1 }</td><td>${ value2 }</td>
+            let th = `<th scope="row">${ item[item_keys_item] }</th>`;
+            let td = `<td>${ item[item_keys_item] }</td>`;
+            return item_keys_index === 0 ? th : td;
+            // return td;
+        };
+        let item_keys = Object.keys(item);
+        let elem = item_keys.map(
+            (item_keys_item, item_keys_index) => render_thtd(item_keys_item, item_keys_index)
+        ).join("");
+        elem = `<tr>${ elem }</tr>`;
+        tbody_data += elem;
+        return;
+    }
+    // Declear vars
+    let thead_data = "";
+    let tbody_data = "";
+    let meta_data = res_data.meta_data;
+    console.log( res_data );
+    // Data render
+    meta_data.map( m => thead_data += `<th scope="col">${ m }</th>` );
+    res_data.data.map( item => render_tbody({ item, tbody_data }) );
+    thead_data = `<tr>${ thead_data }</tr>`;
+    // Put datas
+    $("#response-data table thead").append(thead_data);
+    $("#response-data table tbody").append(tbody_data);
+    // Hidden action
+    $("#ajax-error").attr( "hidden", true );
+    $("#response-data").attr( "hidden", false );
+    return;
+}
+
+function error_msg( data )
+{
+    console.error( data );
+    $("#response-data").attr( "hidden", true );
+    $("#ajax-error").attr( "hidden", false );
+    $("#ajax-error .alert").text( "Data error: " + data.responseJSON.message );
+    return;
 }
 
 function main()
@@ -15,6 +56,7 @@ function main()
         let input = $(".datepicker-here").val();
         let url = "/api/" + input.replace( /-/g , "" );
         let ajax = $.ajax({ url });
-        ajax.done( data => parse_text( data ) );
+        ajax.done( data => parse_text(data) );
+        ajax.error( data => error_msg(data) );
     });
 }
